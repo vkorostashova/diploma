af1b886b1d487b4ee3cc3e67d7f1323db7a54f0c
vkorostashova <viktoria7710@gmail.com>
2023-01-06 20:59:04 +0200

    the end

--- src/components/Communication.js
+++ src/components/Communication.js
@@ -1,34 +1,34 @@
 import React from "react";
 
 function Communication() {
-    return <div>
-        <h1 className={'text-center mt-5 col-12 '}>
-            GET IN TOUCH
-        </h1>
-        <div className={'d-flex mt-5 justify-content-between pb-5'}>
-            <div className={'text-primary mx-1 home-category'} >
-                <h5>Email</h5>
-                <hr />
-                <p>productshop@gmail.com</p>
-            </div>
-            <div className={' text-success mx-1 home-category'}>
-                <h5>Store address</h5>
-                <hr />
-                <p> Houston Induffhhhb,22
-                </p>
-            </div>
-            <div className={'text-danger mx-1 home-category'}>
-                <h5>Call </h5>
-                <hr />
-                <p>015062390, 905155156</p>
-            </div>
-            <div className={' mx-1 home-category'}>
-                <h5>Store opening </h5>
-                <hr />
-                <p>-10 am - 3 pm</p>
+    return <div >
+            <h1 className={'text-center mt-5 col-12 '}>
+                GET IN TOUCH
+            </h1>
+            <div className={'row mt-5 text-center pb-5'}>
+                <div className={'my-1  col-sm-12  col-lg-6 col-xl-3  text-primary   home-category'} >
+                    <h5>Email</h5>
+                    <hr />
+                    <p>productshop@gmail.com</p>
+                </div>
+                <div className={' my-1 col-sm-12  col-lg-6 col-xl-3  text-success  home-category'}>
+                    <h5>Store address</h5>
+                    <hr />
+                    <p> Houston Induffhhhb,22
+                    </p>
+                </div>
+                <div className={'my-1 col-sm-12  col-lg-6 col-xl-3 text-danger  home-category'}>
+                    <h5>Call </h5>
+                    <hr />
+                    <p>015062390, 905155156</p>
+                </div>
+                <div className={'my-1 col-sm-12  col-lg-6 col-xl-3   home-category'}>
+                    <h5>Store opening </h5>
+                    <hr />
+                    <p>-10 am - 3 pm</p>
+                </div>
             </div>
         </div>
-    </div>
 }
 
 export default Communication;
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,44 +9,44 @@ function Home() {
         <hr />
         <div className={'pt-4 text-center col-12 '}>
             <h1>What can you buy in our store</h1>
-            <div className={'d-flex mt-5 '}>
-                <div className={'text-primary mx-1 home-category'} >
+            <div className={'row d-flex mt-5 '}>
+                <div className={'col text-primary m-1 home-category'} >
                     <h5>Men's clothing</h5>
                     <hr />
                     <p>BC Perfect Sweat Technology - high quality fabric for perfect printability.</p>
                 </div>
-                <div className={' text-success mx-1 home-category'}>
+                <div className={'col text-success m-1 home-category'}>
                     <h5>Women's clothing</h5>
                     <hr />
                     <p>Lightweight fabric with great stretch for comfort, Ron sleeves and neckline.
                     </p>
                 </div>
-                <div className={'text-danger mx-1 home-category'}>
+                <div className={' col text-danger m-1 home-category'}>
                     <h5>Jewelery</h5>
                     <hr />
                     <p>Designed and sold by Hafeez Center in the United States. Satisfaction.</p>
                 </div>
-                <div className={' mx-1 home-category'}>
-                    <h5>Electronics</h5>
+                <div className={' col m-1 home-category'}>
+                    <h5>Electronic</h5>
                     <hr />
-                    <p>Lay anywhere Fast and easy, setup Sleek design with high capacity.</p>
+                    <p>Lay anywhere Fast and easy, setup Sleek design with high.</p>
                 </div>
             </div>
             <div className={'my-5 text-center col-12 '}>
                 <h1>Why you should choose our store</h1>
             </div>
-            <div className={'d-flex mt-5 pb-5'}>
-                <div className={'mx-1 home-category'} >
+            <div className={'row d-flex mt-5 pb-5'}>
+                <div className={'col m-1 home-category'} >
                     <h5 className={'text-info'}>More than 10 rolls on the market.</h5>
                     <hr />
                     <h6>More than 500,000 satisfied customers, we take care of our reputation.</h6>
                 </div>
-                <div className={'mx-1 home-category'}>
+                <div className={'col m-1 home-category'}>
                     <h5 className={'text-info'}>We work every day 24/7.</h5>
                     <hr />
                     <h6>The call center works around the clock. You can ask anything.</h6>
                 </div>
-                <div className={'mx-1 home-category'}>
+                <div className={'col m-1 home-category'}>
                     <h5 className={'text-info'}>Free delivery to any city</h5>
                     <hr />
                     <h6>In all stores of the network to branches or at the address.</h6>
